c1db42b79754c3b6ed9b7c03189a4ee8e5b69303
TuNombre <TuEmail@example.com>
2026-05-04 16:38:31 +0200

    añadiendo footer a la web

--- flatsharecliente/src/app/app.ts
+++ flatsharecliente/src/app/app.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Chatbot } from './components/chatbot/chatbot';
-
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Chatbot],
+  imports: [RouterOutlet, Navbar, Chatbot,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
